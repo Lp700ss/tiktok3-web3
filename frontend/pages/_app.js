e489b4b6ef2f4fa4c714dc45ad9347c87d7501e9
Lp700s <siddhant@studio137.co.za>
2022-07-13 15:14:36 +0530

    adding phantom wallet connection

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import dynamic from "next/dynamic";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import { WalletConnectButton } from "@solana/wallet-adapter-react-ui";
+import WalletConnectionProvider from "../context/WalletConnectionProvider";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const WalletConnectionProvider = dynamic(
+    () => import("../context/WalletConnectionProvider"),
+    { ssr: false }
+  );
+
+  return;
+  <WalletConnectionProvider>
+    <Component {...pageProps} />
+  </WalletConnectionProvider>;
 }
 
-export default MyApp
+export default MyApp;
